a9cb73e03fe8fbb9061d1cda870bcdee211699df
Min Xu <min.mxu@gmail.com>
2019-03-26 06:37:42 -0700

    update the script to create venv

--- make_ilamb_venv.sh
+++ make_ilamb_venv.sh
@@ -34,8 +34,49 @@ if [ x$useconda = 'xTrue' ]; then
    touch ${ilamb_venv_dir}/${CONF_name}/etc/conda/deactivate.d/env_var.sh
 
 
-   echo "export ILAMB_VENV_DIR=${ilamb_venv_dir}" >> ${ilamb_venv_dir}/$CONF_name/etc/conda/activate.d/env_var.sh
-   echo "unset ILAMB_VENV_DIR" >> ${ilamb_venv_dir}/$CONF_name/etc/conda/deactivate.d/env_var.sh
+   cat <<EOF > ${ilamb_venv_dir}/$CONF_name/etc/conda/activate.d/env_var.sh
+
+#!/usr/bin/env bash
+
+if [[ ! -n $ILAMB_VENV_DIR ]]; then
+   export ILAMB_VENV_DIR=${ilamb_venv_dir}
+fi
+export PYTHONNOUSERSITE=1
+
+if [[ ! -n $ILAMB_VENV_HOST ]]; then
+    export ILAMB_VENV_HOST=$host
+fi
+
+if [[ ! -n $ILAMB_VENV_GROUP ]]; then
+    export ILAMB_VENV_HOST=$group
+fi
+
+if [[ $host == *"edison"* || $host == *"cori"*  ]]; then
+    export HDF5_USE_FILE_LOCKING=FALSE
+fi
+
+EOF
+
+   cat <<EOF > ${ilamb_venv_dir}/$CONF_name/etc/conda/deactivate.d/env_var.sh
+#!/usr/bin/env bash
+
+if [[ -n $ILAMB_VENV_DIR ]]; then
+   unset ILAMB_VENV_DIR
+fi
+unset PYTHONNOUSERSITE
+
+if [[ -n $ILAMB_VENV_HOST ]]; then
+  unset ILAMB_VENV_HOST
+fi
+
+if [[ -n $ILAMB_VENV_GROUP ]]; then
+  unset ILAMB_VENV_GROUP
+fi
+
+if [[ $host == *"edison"* || $host == *"cori"*  ]]; then
+    unset HDF5_USE_FILE_LOCKING=FALSE
+fi
+EOF
 
    # fix mpi4py error
    #-if [ x$fxmpi4py = 'xTrue' ]; then
